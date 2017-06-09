66f231588f58f3fbb0c1cdcee219084f3a94c7df
sabrina-wen <sabrinawn253@gmail.com>
2017-06-09 01:31:53 -0400

    not sure why this was not added before

---
+++ PACMAN/Dot.java
@@ -0,0 +1,20 @@
+public class Dot {
+
+    private boolean isFruit;
+    private boolean isBig;
+
+    public Dot (boolean fruit, boolean big) {
+	isFruit = fruit;
+	isBig = big; 
+    }
+
+    public boolean isFruit () {
+	return isFruit;
+    }
+
+    public boolean isBig () {
+	return isBig;
+    }
+
+    
+} 
---
+++ PACMAN/Wall.java
@@ -0,0 +1,12 @@
+public class Wall extends Location {
+
+    public Wall (int r, int c) {
+	super (r, c);
+    }
+
+    public boolean isValid () {
+	    return false;
+    } 
+
+
+} 
